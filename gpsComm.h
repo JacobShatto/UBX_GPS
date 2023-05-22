98a319ccebc29b41bbb7578abbd0c16c7d03659d
andrewmcg1 <greekgeek191@gmail.com>
2023-05-22 12:59:54 -0400

    Imroved commenting

--- gpsComm.h
+++ gpsComm.h
@@ -2,8 +2,8 @@
  * @file gpsComm.h
  * @author Jacob Shatto and Andrew McGrellis
  * 
- * @brief Implementation of u-blox M10 SPG 5.00, based on the following documetation:
- * https://content.u-blox.com/sites/default/files/u-blox%20M10-SPG-5.00_InterfaceDescription_UBX-20053845.pdf
+ * @brief Implementation of u-blox M10 SPG 5.00, for M10 GPS modules 
+ * Based on the following documetation: https://content.u-blox.com/sites/default/files/u-blox%20M10-SPG-5.00_InterfaceDescription_UBX-20053845.pdf
  * 
  * @date 2023-05-22
  * 
@@ -16,11 +16,25 @@
 #include <stdint.h>
 #include <stdio.h>
 
+ /**
+  * GPS comm defines
+ */
+#define PREAMBLE_SYNC_CHAR_1 0xb5
+#define PREAMBLE_SYNC_CHAR_2 0x62
+
+ /**
+  * UART defines
+ */
+#define GPS_BUFFER_SIZE 64
+#define GPS_TIMEOUT 0.5
+#define GPS_BAUDRATE 115200
+#define GPS_BUS 2
+
+
 /**
- * GPS data frame
+ * @brief GPS data frame
  * 
  */
-
 typedef struct gps_body {
 
     uint8_t syncChar1; // start bits of frame
@@ -137,23 +151,6 @@ typedef struct gps_body {
     uint8_t checksum[2]; // checksum of payload
 } GPS_frame;
 
-/**
- * gpsComm.h
- */
-
- /**
-  * GPS comm defines
- */
-#define PREAMBLE_SYNC_CHAR_1 0xb5
-#define PREAMBLE_SYNC_CHAR_2 0x62
-
- /**
-  * UART defines
- */
-#define GPS_BUFFER_SIZE 64
-#define GPS_TIMEOUT 0.5
-#define GPS_BAUDRATE 115200
-#define GPS_BUS 2
 
 uint8_t buffer[GPS_BUFFER_SIZE];
 
@@ -174,26 +171,26 @@ typedef enum _PacketType PacketType;
 
 
 /**
- * Function to read and parse data
+ * @brief Function to read and parse GPS data
  *
  * @return 0 on success -1 on failure
  */
 int GPS_readData();
 
 /**
- * Function to initialize GPS communication
+ * @brief Function to initialize GPS communication
  *
  * @return 0 on success -1 on failure
 */
 int GPS_init();
 
 /**
- * Function to allocate space fot eht incoming message
+ * @brief Function to allocate space fot the incoming message
  */
 void GPS_allocatePayloadMemory();
 
 /**
- * Function to update the gps stage.
+ * @brief Function to update the gps stage.
  *
  * Usage: Only use when
  *
